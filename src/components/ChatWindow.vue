211cab7826a7b7685033b7a5b0be5567789e11d5
Yavor Spassov <ygspasov@gmail.com>
2020-04-12 10:33:13 +0300

    Adding Moment and Vue Chat Scroll

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,27 +1,31 @@
 <template>
   <v-container id="chat-window">
     <v-row>
-      <h1 class="mx-auto mb-3">Chat {{ name }}</h1>
+      <h1 class="mx-auto mb-3">Chat as {{ name }}</h1>
     </v-row>
-    <v-row>
+    <v-row id="messages" v-chat-scroll>
       <v-card class="mx-auto" max-width="344">
-        <v-card-text>
-          <p class="text--primary">Name</p>
-          <p>Message</p>
-          <p>Time</p>
+        <v-card-text v-if="messages">
+          <ul class="pl-0 mb-2" v-for="(msg, i) in messages" :key="i">
+            <li>
+              <p>
+                <span class="title">{{ msg.name }}:</span>
+                <span class="subtitle-1 pl-1">{{ msg.message }}</span>
+              </p>
+              <p class="subtitle-2">{{ msg.time }}</p>
+            </li>
+          </ul>
         </v-card-text>
-        <v-textarea
+        <v-text-field
           class="mx-4"
           name="input-7-1"
           label="Enter message"
           hint="Type here"
           v-model="message"
-        ></v-textarea>
+        ></v-text-field>
         <p class="errorMsg mx-4 red--text">{{ this.error }}</p>
         <v-card-actions>
-          <v-btn text color="deep-purple accent-4" @click="addMessage"
-            >Send</v-btn
-          >
+          <v-btn text color="deep-purple accent-4" @click="addMessage">Send</v-btn>
         </v-card-actions>
       </v-card>
     </v-row>
@@ -29,13 +33,15 @@
 </template>
 <script>
 import db from "../db";
+import moment from "moment";
 export default {
   name: "ChatWindow",
   props: ["name"],
   data() {
     return {
       message: "",
-      error: "",
+      messages: [],
+      error: ""
     };
   },
   methods: {
@@ -43,18 +49,43 @@ export default {
       if (this.message) {
         db.collection("messages")
           .add({
-            name,
+            name: this.name,
             content: this.message,
-            time: Date.now(),
+            time: moment().format("lll")
           })
-          .catch((err) => console.log(err));
+          .catch(err => console.log(err));
         console.log(this.message);
         this.message = "";
         this.error = "";
       } else {
         this.error = "You must enter a message";
       }
-    },
+    }
   },
+  created() {
+    let messages = db.collection("messages").orderBy("time");
+    messages.onSnapshot(data => {
+      data.docChanges().forEach(change => {
+        if (change.type === "added") {
+          console.log("New record: ", change.doc.data());
+          this.messages.push({
+            id: change.doc.id,
+            name: change.doc.data().name,
+            message: change.doc.data().content,
+            time: change.doc.data().time
+          });
+        }
+      });
+    });
+  }
 };
 </script>
+<style scoped>
+ul {
+  list-style-type: none;
+}
+#messages {
+  max-height: 600px;
+  overflow: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
+import VueChatScroll from "vue-chat-scroll";
+
+Vue.use(VueChatScroll);
 
 Vue.config.productionTip = false;
 
